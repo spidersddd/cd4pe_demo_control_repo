83e2157532fb2c63d10a936870909ea7b7d7ff9d
spidersddd <troy.klein@puppet.com>
2020-01-09 18:18:01 -0600

    fix for banner

--- site-modules/profile/manifests/os/baseline/banner.pp
+++ site-modules/profile/manifests/os/baseline/banner.pp
@@ -1,8 +1,10 @@
 # This class will setup MOTD for Windows and Linux hosts
 class profile::os::baseline::banner (
   String $motd,
+  String $windows_motd_title,
 ) {
   class { 'motd':
-    content => $motd,
+    content            => $motd,
+    windows_motd_title => $windows_motd_title,
   }
 }
